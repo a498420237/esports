c833e8b5e97b9b44c3cb2f2920d6fcf7880f1eee
sharp5631 <541014729@qq.com>
2018-04-27 21:37:32 +0800

    详情页面数据接入

--- src/main/resources/static/scripts/controller/competition/info.js
+++ src/main/resources/static/scripts/controller/competition/info.js
@@ -2,42 +2,63 @@ define(function(require, exports, module) {
 	
 	seajs.use([ 'jquery', 'pagePlugin', 'utilService' ], function($,pagePlugin, util) {
 		
-		$("#againstInfoList").paginator({
-			itemTemplateId : 'againstInfo_template',
-			pageNavId : 'pageContainer',
-			usepager:false,
-			ajaxFuc : function(curentPage, renderHtml) {
-//				var gameType = $("#competitionTab .nav.active").data("type");
-				$.ajax({
-					url : "/competition/againstInfo/"+11+"?num=-1",
-					datatype : 'json',
-					type : "get",
-					data : {
-					},
-					success : function(json) {debugger;
-						var data = json.t;
-						var gameDayList = data.gameDayList;
-						gameDayList = gameDayList.sort(function(a,b){//排序
-					        return a.num - b.num;
-					    });  
-					    
-						var paramObj = {
-							 list : gameDayList
-						};
-						renderHtml(paramObj);
-						
-						$(".saishi_process .process_item").each(function(){
-							var num=$(".saishi_process .process_item").length;
-							$(this)[0].style.width=100/num+"%"	
-						})
-						$(".saishi_process .process_item").click(function(){
-							$(this).addClass("active").siblings().removeClass("active");
-						})
-					}
-				});
-			}
-		});
+		var loadData = function(num){
+			$.ajax({
+				url : "/competition/againstInfo/"+11+"?num="+num,
+				datatype : 'json',
+				type : "get",
+				data : {
+				},
+				success : function(json) {
+					var data = json.t;
+					var gameDayList = data.gameDayList;
+					gameDayList = gameDayList.sort(function(a,b){//排序
+				        return a.num - b.num;
+				    });  
+					
+			    	var currGameDay = "";
+			    	var currNum = num == 0 ? gameDayList.length - 1 : 0;
+		    		var currGameDay = gameDayList[currNum];
+		    		currGameDay.dataClass = "active";
+		    		
+			    	var againstPlanList = currGameDay.againstPlanList;	
+			    	var gameDay = new Date(currGameDay.gameDay).format("yyyy-MM-dd");
+			    	for(var index in againstPlanList){
+			    		var againstPlan = againstPlanList[index];
+			    		againstPlan.gameDay = gameDay;
+			    		if(againstPlan.winId != undefined){
+			    			againstPlan.statuClass =  "end";
+			    			againstPlan.statuName = "已结束";
+			    		}else{
+			    		   againstPlan.statuClass =  "runing";
+			    		   againstPlan.statuName = "正在进行";
+			    		}
+			    	}
+					
+			    	if(num == 0){//初始化时
+			    		util.fillHtml("againstInfoList", $("#againstInfo_template").val(), gameDayList);
+			    	}
+			    	util.fillHtml("againstPlans", $("#againstPlan_template").val(), againstPlanList);
+			    	pageInit();
+				}
+			});
+		}
 		
+		function pageInit(){
+			$(".saishi_process .process_item").each(function(){
+				var num=$(".saishi_process .process_item").length;
+				$(this)[0].style.width=100/num+"%"	
+			})
+			$(".saishi_process .process_item").click(function(){
+				$(this).addClass("active").siblings().removeClass("active");
+			})
+		}
+		
+		loadData(0);
+		$("#againstInfoList").on("click", ".process_item", function(){
+			var num = $(this).data("num");
+			loadData(num);
+		});
 	});
 	
 	
--- src/main/resources/static/scripts/services/util-service.js
+++ src/main/resources/static/scripts/services/util-service.js
@@ -9,24 +9,34 @@ define(function(require, exports, module) {
 	String.prototype.aa = function(obj) {
 	    alert(obj);
 	};
+	//日期原型  格式化方法    fmt 格式 如：yyyy-MM-dd
+	Date.prototype.format = function (fmt) {
+		var o = {    
+				"M+": this.getMonth() + 1, //月份     
+				"d+": this.getDate(), //日     
+				"H+": this.getHours(), //小时     
+				"m+": this.getMinutes(), //分     
+				"s+": this.getSeconds(), //秒     
+				"q+": Math.floor((this.getMonth() + 3) / 3), //季度     
+				"S": this.getMilliseconds() //毫秒     
+		};    
+		if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));    
+		for (var k in o)    
+			if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));    
+		return fmt;    
+	};    
+	
+	
 	exports.bb=function(bb){
 		alert(bb);
 	};
 	
-	//日期原型  格式化方法    fmt 格式 如：yyyy-MM-dd
-	Date.prototype.format = function (fmt) {
-	    var o = {    
-	        "M+": this.getMonth() + 1, //月份     
-	        "d+": this.getDate(), //日     
-	        "H+": this.getHours(), //小时     
-	        "m+": this.getMinutes(), //分     
-	        "s+": this.getSeconds(), //秒     
-	        "q+": Math.floor((this.getMonth() + 3) / 3), //季度     
-	        "S": this.getMilliseconds() //毫秒     
-	    };    
-	    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));    
-	    for (var k in o)    
-	    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));    
-	    return fmt;    
-	};    
+	//填充模板方法
+	exports.fillHtml = function(domId, templateHtml, data){
+		var content = "";
+		data.forEach(function(obj) {
+			content += templateHtml.repalceVars(obj);
+		});
+		$("#"+domId).html(content);
+	}
 });
