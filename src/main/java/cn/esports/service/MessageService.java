6e30782ee844fa71e7c9cb769f407b3b2d7d308b
zhengfengzhang <1434383597@qq.com>
2018-04-27 11:41:35 +0800

    资讯部分功能提交

---
+++ src/main/java/cn/esports/service/MessageService.java
@@ -0,0 +1,42 @@
+package cn.esports.service;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+
+import java.util.Map;
+
+@Component
+public class MessageService extends BaseService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MessageService.class);
+
+	public JSONObject getInformations(Map<String, String> uriVariables) {
+		try {
+			return restTemplate.getForObject(createUrl("/app/infoContentSearch.json", uriVariables),JSONObject.class);
+		} catch (RestClientException e) {
+			logger.error("get the message list from rest api occurred error,cause by:",e);
+			return null;
+		}
+	}
+
+	public JSONObject getTitles(Map<String, String> uriVariables) {
+		try {
+			return restTemplate.getForObject(createUrl("/app/findInfoChannelNameForApp.json", uriVariables),JSONObject.class);
+		} catch (RestClientException e) {
+			logger.error("get the message list from rest api occurred error,cause by:",e);
+			return null;
+		}
+	}
+
+	public JSONObject getMessageDetail(Map<String, String> uriVariables) {
+		try {
+			return restTemplate.getForObject(createUrl("/app/findInfoContentListForApp.json", uriVariables),JSONObject.class);
+		} catch (RestClientException e) {
+			logger.error("get the message list from rest api occurred error,cause by:",e);
+			return null;
+		}
+	}
+}
