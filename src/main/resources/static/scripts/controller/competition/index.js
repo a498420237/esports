e107bf1dc61470a85f6053d056414f52cb4ee5cb
liukunrui <541014729@qq.com>
2018-04-27 13:04:35 +0800

    赛事分页

--- src/main/resources/static/scripts/controller/competition/index.js
+++ src/main/resources/static/scripts/controller/competition/index.js
@@ -8,41 +8,47 @@ define(function(require, exports, module) {
 		"5" : "end",	
 	};
 	
+	function loadDatas(curentPage, renderHtml){
+		var gameType = $("#competitionTab .nav.active").data("type");
+		$.ajax({
+			url : "/competition/list",
+			datatype : 'json',
+			type : "get",
+			data : {
+				"offset" : curentPage,
+				"limit" : 4,
+				"gameType" : gameType
+				
+			},
+			success : function(json) {
+				var data = json.t;
+				var results = data.result;
+				for(var index in results){
+					var r = results[index];							
+					r.matchStatusClass = matchStatusClass[r.matchStatus];
+				}
+				
+				var paramObj = {
+					total : data.total,
+					page : curentPage,
+					list : data.result
+				};
+				renderHtml(paramObj);
+			}
+		});
+	}
+	
 	seajs.use([ 'jquery', 'pagePlugin', 'utilService' ], function($,pagePlugin, util) {
 		
 		$("#competitionList").paginator({
 			itemTemplateId : 'competitionList_template',
 			pageNavId : 'pageContainer',
 			ajaxFuc : function(curentPage, renderHtml) {
-				
-				var gameType = $("#competitionTab .nav.active").data("type");
-				$.ajax({
-					url : "/competition/list",
-					datatype : 'json',
-					type : "get",
-					data : {
-						"offset" : curentPage,
-						"limit" : 4,
-						"gameType" : gameType
-						
-					},
-					success : function(json) {
-						var data = json.t;
-						var results = data.result;
-						for(var index in results){
-							var r = results[index];							
-							r.matchStatusClass = matchStatusClass[r.matchStatus];
-						}
-						
-						var paramObj = {
-							total : data.total,
-							page : data.page,
-							list : data.result
-						};
-						renderHtml(paramObj);
-					}
-				});
+				loadDatas(curentPage, renderHtml);
 			}
 		});
+		
 	});
+	
+	
 });
