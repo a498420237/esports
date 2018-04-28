10625a17c8d9c72631a05c8f949cd01945102ba9
liukunrui <541014729@qq.com>
2018-04-28 14:47:39 +0800

    因为接口没有状态参数 所以在内存中临时过滤分页 后续接口完善再直接去掉即可

--- src/main/java/cn/esports/service/CompetitionService.java
+++ src/main/java/cn/esports/service/CompetitionService.java
@@ -1,6 +1,8 @@
 package cn.esports.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -8,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -31,11 +34,58 @@ public class CompetitionService extends BaseService {
 	 */
 	public JSONObject getCompetitions(Map<String, String> uriVariables) {
 		try {
-			return restTemplate.getForObject(createUrl(LIST_URL, uriVariables),JSONObject.class);
+//			return restTemplate.getForObject(createUrl(LIST_URL, uriVariables),JSONObject.class);
+			
+			/*因为接口没有 状态 参数   所以如果要体现效果只能内存中过滤
+			 * 以下是简单的内存过滤实现    后续接口完善了  直接使用上面的一行代码即可
+			 */
+			Integer offSet = Integer.parseInt(uriVariables.get("offset"));
+			Integer limit = Integer.parseInt(uriVariables.get("limit"));
+			String gameType = uriVariables.get("gameType");
+			String statuType = uriVariables.get("statuType");
+			uriVariables.remove("limit");//调接口不分页
+			
+			JSONObject result = restTemplate.getForObject(createUrl(LIST_URL, uriVariables),JSONObject.class);
+			JSONObject t = result.getJSONObject(("t"));
+			JSONArray results = t.getJSONArray("result");
+			List<JSONObject> matchs = new ArrayList<JSONObject>();
+			for(int i=0; i<results.size();i++){
+				JSONObject o = results.getJSONObject(i);
+				if(matchGameType(gameType, o) && matchStatuType(statuType, o)){
+					matchs.add(o);
+				}
+			}
+
+			//分页
+			int start = (offSet - 1) * limit;
+			if(start < 0 || start > matchs.size()){
+				start = 0;
+			}
+			int toIndex = start + limit;
+			if(toIndex > matchs.size()){
+				toIndex = matchs.size();
+			}
+			List<JSONObject> subList = matchs.subList(start, toIndex);    
+			
+			JSONObject ret0 = new JSONObject();
+			ret0.put("result", subList);
+			ret0.put("total", matchs.size());
+			JSONObject ret = new JSONObject();
+			ret.put("t", ret0);
+			return ret;
 		} catch (RestClientException e) {
 			logger.error("call the forecast list from rest api occurred error,cause by:",e);
 			return null;
 		}
+	}
+	
+	private boolean matchStatuType(String statuType, JSONObject o) {
+		String matchStatus = o.getString("matchStatus") ;
+		return statuType.equals(matchStatus);
+	}
+
+	private boolean matchGameType(String gameType, JSONObject o) {
+		return "-1".equals(gameType) || gameType.equals(o.getString("gameType"));
 	}
 	
 	/**
